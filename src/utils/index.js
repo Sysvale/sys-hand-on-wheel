bdddfef4b26017708a24b5d12da3737ed9a422bf
Tássio Caique <tassiok2@gmail.com>
2022-01-18 09:43:26 -0300

    feat: generateKey e removeAccents como helpers

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export { default as convertKeysToCamelCase } from './convertKeysToCamelCase';
+export { default as convertKeysToSnakeCase } from './convertKeysToSnakeCase';
+export { default as getFirstErrorMessage } from './getFirstErrorMessage';
+export { default as removeAccents } from './removeAccents';
+export { default as generateKey } from './generateKey';
