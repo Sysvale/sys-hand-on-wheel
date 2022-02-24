20bf4f8b42403dee688238864a097588790c3631
lucasn4s <lucas.barbosa.nas@gmail.com>
2022-02-24 10:21:11 -0300

    Cria setup de testes

--- src/components/__tests__/RequestProvider.spec.js
+++ src/components/__tests__/RequestProvider.spec.js
@@ -1,13 +1,27 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import flushPromises from 'flush-promises';
-
+import convertKeysToCamelCase from '../../utils/convertKeysToCamelCase';
+import convertKeysToSnakeCase from '../../utils/convertKeysToSnakeCase';
 import RequestProvider from '../RequestProvider.vue';
 
 const localVue = createLocalVue();
 
-const serviceMock = (payload) => Promise.resolve({
-	data: payload,
-});
+const responseDataMock = {
+	message: 'success message',
+};
+
+const payloadMock = {
+	id: '1',
+};
+
+const successfulServiceMock = jest.fn(() => Promise.resolve({
+	data: responseDataMock,
+}));
+const failedServiceMock = jest.fn(() => Promise.reject(Error('test error')));
+const payloadResolverMock = jest.fn((payload) => convertKeysToCamelCase(payload));
+const dataResolverMock = jest.fn((data) => convertKeysToSnakeCase(data));
+const successFeedbackResolverMock = jest.fn();
+const errorFeedbackResolverMock = jest.fn();
 
 test('Component renders correctly', async () => {
 	const wrapper = mount(RequestProvider, {
@@ -16,7 +30,7 @@ test('Component renders correctly', async () => {
 			default: '<div />',
 		},
 		propsData: {
-			service: serviceMock,
+			service: successfulServiceMock,
 		},
 	});
 
