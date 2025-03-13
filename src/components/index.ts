24f298b2462c39aeabd1dd40730d389553271d90
Tássio Caique <tassiok2@gmail.com>
2025-03-13 17:00:42 -0300

    feat: registra e expõe componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,9 @@
 import RequestProvider from './RequestProvider.vue';
 import RequestObserver from './RequestObserver.vue';
 import RequestSelect from './RequestSelect.vue';
+import FormWizard from './FormWizard.vue';
+import Form from './Form.vue';
+import Field from './Field.vue';
 /** -------*/
 
 /** Utils */
@@ -11,15 +14,24 @@ import {
     removeAccents,
     generateKey,
     toThousands,
+    setupVeeValidateWrapper,
 } from '../utils';
 /* -------*/
 
 
 export default {
-    install(app: any) {
+    install(app: any, options = {
+        veeValidateOptions: {},
+    }) {
+        const { veeValidateOptions } = options;
+        const veeValidateWrapper = setupVeeValidateWrapper(veeValidateOptions);
+
         app.component('ShowRequestProvider', RequestProvider);
         app.component('ShowRequestObserver', RequestObserver);
         app.component('ShowRequestSelect', RequestSelect);
+        app.component('ShowForm', Form);
+        app.component('ShowField', Field);
+        app.component('ShowFormWizard', FormWizard);
 
 
         const utils = {
@@ -29,17 +41,21 @@ export default {
             $showGenerateKey: generateKey,
             $showToThousands: toThousands,
         };
+
         const version = Number(app.version.split('.')[0]);
-    
+        
         if (version <= 2) {
             throw new Error('Essa versão só é compatível com projetos que possuem o Vue 3. Para projetos com a Vue 2, utilize a versão 0.3.0 ou inferior');
         }
-    
+        
         if (version > 2) {
             // ficará disponível apenas com o uso do Options API
             Object.keys(utils).forEach((key: string) => {
                 app.config.globalProperties[key] = utils[key];
             });
+
+            // wrapper do vee-validate  
+            app.config.globalProperties.$showVeeValidate = veeValidateWrapper;
         }
     },
 }
