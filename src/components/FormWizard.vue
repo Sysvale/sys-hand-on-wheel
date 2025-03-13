8f96a82674c03740e761c158e9b61455df472372
Tássio Caique <tassiok2@gmail.com>
2025-03-13 17:00:30 -0300

    feat: implementa FormWizard

---
+++ src/components/FormWizard.vue
@@ -0,0 +1,244 @@
+<template>
+	<div class="form-wizard">
+		<cds-stepper
+			:steps="computedHeaders"
+			:value="currentHeaderStep"
+		/>
+		<cds-spacer :margin-bottom="4" />
+		<template
+			v-for="step in internalSteps"
+			:key="step.id"
+		>
+			<Form
+				v-show="step.id === currentStepId"
+				keep-values
+				:ref="(el) => forms[step.id] = el"
+				:initial-values="model[step.id]"
+			>
+				<component
+					:is="step.component"
+				/>
+			</Form>
+		</template>
+		<cds-spacer :margin-bottom="4" />
+		<cds-flexbox
+			gap="2"
+		>
+			<cds-button
+				:disabled="isFirstStep || disablePreviousButton"
+				secondary
+				@click="goToPreviousStep"
+			>
+				Anterior
+			</cds-button>
+			<cds-button
+				:disabled="disableNextButton"
+				@click="handleNextStep"
+			>
+				{{ nextButtonLabelResolver(isLastStep) }}
+			</cds-button>
+		</cds-flexbox>
+	</div>
+</template>
+  
+<script setup>
+import { Form } from 'vee-validate';
+import { ref, computed, watch, onMounted, defineEmits } from 'vue';
+import keyBy from 'lodash.keyby';
+
+const props = defineProps({
+	steps: {
+		type: Array,
+		required: true,
+	},
+	initialStepId: {
+		type: String,
+		default: null,
+	},
+	disableNextButton: {
+		type: Boolean,
+		default: false,
+	},
+	disablePreviousButton: {
+		type: Boolean,
+		default: false,
+	},
+	nextButtonLabelResolver: {
+		type: Function,
+		default: (isLastStep) => isLastStep ? 'Enviar' : 'Próximo',
+	},
+	previousButtonLabelResolver: {
+		type: Function,
+		default: (isFirstStep) => isFirstStep ? 'Anterior' : 'Anterior',
+	},
+});
+
+const emit = defineEmits(['next', 'previous']);
+
+const model = defineModel();
+const forms = ref({});
+const headers = ref([]);
+const validationState = ref({});
+const currentStepId = ref(props.initialStepId || props.steps[0].id);
+const internalSteps = computed(() => props.steps.map((step) => ({
+		...step,
+		$nextStep: () => {
+			const nextStepId = typeof step.nextStep === 'function'
+				? step.nextStep(model.value)
+				: step.nextStep || getNextStepIdFromArray(step.id);
+
+			return stepMap.value[nextStepId];
+		},
+})));
+
+const stepMap = computed(() => keyBy(internalSteps.value, 'id'));
+
+const computedHeaders = computed(() => {
+	return headers.value.map((header) => {
+		const validated = validationState.value && validationState.value[header.id]?.validated;
+		const valid = validationState.value && validationState.value[header.id]?.valid;
+		return {
+			...header,
+			completed: validated && valid,
+			error: validated && !valid,
+			inProcessing: !validated && header.id === currentStepId.value,
+		}
+	});
+});
+
+const currentStep = computed(() => stepMap.value[currentStepId.value]);
+
+const currentHeaderStep = computed(() => headers.value.findIndex(({ id }) => currentStepId.value === id) + 1);
+
+const progress = computed(() => calculateProgress());
+  
+const isFirstStep = computed(() => currentStepId.value === internalSteps.value[0].id);
+  
+const isLastStep = computed(() => {
+	if (currentStep.value.$nextStep() === 'end') return true;
+	const currentIndex = internalSteps.value.findIndex((step) => step.id === currentStepId.value);
+	return currentIndex === internalSteps.value.length - 1;
+});
+
+watch(() => forms.value[currentStepId.value]?.values, (newValues) => {
+	const updatedModel = {
+		...model.value,
+		[currentStepId.value]: newValues,
+	};
+
+	const allowedKeys = headers.value.map(({ id }) => id);
+	const filteredModel = allowedKeys.reduce((acc, key) => {
+		acc[key] = updatedModel[key];
+		return acc;
+	}, {});
+
+	model.value = { ...filteredModel };
+
+}, { immediate: true, deep: true });
+
+onMounted(() => {
+	buildHeaders();
+	initializeValues();
+});
+
+const buildHeaders = () => {
+	headers.value = [];
+
+	const firstStepId = props.steps[0].id;
+	let step = stepMap.value[firstStepId];
+
+	while (step && step.$nextStep() !== 'end') {
+		insertHeader(step);
+		step = step.$nextStep();
+	}
+	console.log(step)
+	if(step?.$nextStep() === 'end') {
+		insertHeader(step);
+	}
+};
+
+const insertHeader = ({ label, id }) => headers.value.push({ label, id });
+
+const initializeValues = () => {
+	headers.value.forEach((header) => {
+		model.value = {
+			...model.value,
+			[header.id]: forms.value[header.id]?.values,
+		};
+
+		validationState.value = {
+			...validationState.value,
+			[header.id]: {
+				validated: false,
+				valid: false,
+			},
+		};
+	});
+}
+  
+const calculateProgress = () => {
+	const steps = internalSteps.value;
+	let totalSteps = 0;
+	let currentStepIndex = 0;
+
+	let step = stepMap.value[steps[0].id];
+	while(step) {
+		console.log('calculateProgress');
+		totalSteps++;
+
+		if (step.id === currentStepId.value) {
+			currentStepIndex = totalSteps;
+		}
+
+		if(step.$nextStep().id === 'end') break;
+		step = step.$nextStep();
+	}
+
+	return totalSteps === 0 ? 0 : (currentStepIndex / totalSteps) * 100;
+}
+  
+const handleNextStep = async () => {
+	const state = await forms.value[currentStepId.value].validate();
+
+	validationState.value[currentStepId.value] = {
+		validated: true,
+		valid: state.valid,
+	};
+
+	if(state.valid) {
+		buildHeaders();
+		goToNextStep();
+		emit('next', currentStep);
+		if(isLastStep.value) {
+			emit('submit', model.value);
+		}
+	}
+}
+
+const goToNextStep = () => {
+	currentStepId.value = currentStep.value.$nextStep().id;
+	headers.value.find((step) => step.id === currentStepId.value).inProcessing = true;
+}
+  
+const goToPreviousStep = () => {
+	const previousStep = internalSteps.value.find((step) => step.$nextStep().id === currentStepId.value);
+	if (previousStep) {
+		currentStepId.value = previousStep.id;
+		emit('previous', currentStep);
+	}
+}
+
+const getNextStepIdFromArray = (currentStepId) => {
+	const steps = internalSteps.value;
+	const currentIndex = steps.findIndex((step) => step.id === currentStepId);
+	if (currentIndex < steps.length - 1) {
+		return steps[currentIndex + 1].id;
+	}
+	return 'end';
+}
+
+defineExpose({
+	progress,
+});
+</script>
+
