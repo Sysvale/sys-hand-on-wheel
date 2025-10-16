e71b27ce99da2991fa7ba7b922ca3b249a9145ac
Tássio Caique <tassiok2@gmail.com>
2025-10-16 09:09:59 -0300

    fix: ajusta proxies do field

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,34 +1,29 @@
 <template>
-	<VeeField
+  <VeeField
     v-bind="$attrs"
-    v-slot="{ field, errors }"
+    v-slot="slotProps"
     :validate-on-change="validateOnChange"
     :validate-on-model-update="validateOnModelUpdate"
     v-on="$listeners"
   >
-	  <slot
-      :field="{
-        ...field,
-        modelValue: field.value,
-        errorMessage: errors[0],
-        state: errors[0] ? 'invalid' : 'default'
+    <slot
+      v-bind="{
+        ...slotProps,
+        field: {
+          ...slotProps.field,
+          errorMessage: slotProps.errors[0],
+          state: slotProps.errors[0] ? 'invalid' : 'default'
+        }
       }"
-      :errors="errors"
     />
-	</VeeField>
-  </template>
-  
-  <script setup>
-  import { Field as VeeField } from 'vee-validate';
+  </VeeField>
+</template>
 
-  const props = defineProps({
-    validateOnChange: {
-      type: Boolean,
-      default: true
-    },
-    validateOnModelUpdate: {
-      type: Boolean,
-      default: true
-    },
-  });
-  </script>
+<script setup>
+import { Field as VeeField } from 'vee-validate';
+
+const props = defineProps({
+  validateOnChange: { type: Boolean, default: true },
+  validateOnModelUpdate: { type: Boolean, default: true },
+});
+</script>
